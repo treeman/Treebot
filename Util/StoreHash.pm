ad38b6c6fd1b27bcc767253b37b3431a041162b5
Jonas Hietala <tradet.h@gmail.com>
2011-04-18 22:53:44 +0200

    Store hash of hashes.

--- Util/StoreHash.pm
+++ Util/StoreHash.pm
@@ -6,7 +6,7 @@ package StoreHash;
 
 use Carp;
 
-sub store
+sub store_simple
 {
     my ($name, %h) = @_;
 
@@ -21,7 +21,7 @@ sub store
     close $fh;
 }
 
-sub retrieve
+sub retrieve_simple
 {
     my ($name) = @_;
 
@@ -44,6 +44,80 @@ sub retrieve
     }
 
     return %h;
+}
+
+# Store a hash of hashes in a very easy to read manner
+sub store_hash_hash
+{
+    my ($name, $h) = @_;
+
+    my $file = "Data/$name";
+
+    open my $fh, '>', $file or croak "Couldn't open file: $file";
+
+    for my $key (keys %$h) {
+        my $hashes = "";
+        my $a = $h->{$key};
+
+        print $fh "$key:\n";
+
+        my @vals;
+        while (my ($key, $val) = (each %$a)) {
+            print $fh "  $key: $val\n";
+        }
+    }
+
+    close $fh;
+}
+
+# Retrieve said hash of hashes
+sub retrieve_hash_hash
+{
+    my ($name) = @_;
+
+    my $file = "Data/$name";
+
+    my $outer;
+    if (-e $file) {
+        open my $fh, '<', $file or croak "Couldn't open file: $file";
+
+        my $outer_key = "";
+        my $inner = {};
+        my $indent_lvl = 0;
+        while (<$fh>) {
+            $_ =~ /^(\s*)([^:]+):\s+(.*)/;
+            my $spaces = 0;
+            if ($1) { $spaces = length $1; }
+            my $name = $2;
+            my $val;
+            if ($3) {
+                $val = $3;
+            }
+            else {
+                $val = "";
+            }
+
+            # New big time hash
+            if ($spaces == 0) {
+                if ($outer_key) {
+                    $$outer{$outer_key} = $inner;
+                }
+                $inner = {};
+                $outer_key = $name;
+            }
+            # Continue setting our inner hash
+            else {
+                $inner->{$name} = $val;
+            }
+        }
+        if ($outer_key) {
+            $$outer{$outer_key} = $inner;
+        }
+
+        close $fh;
+    }
+
+    return $outer;
 }
 
 1;
